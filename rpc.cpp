8cea9f1fd7d1a6e26524c96f42027b6b71ff5d77
AuraCoder-95 <zkbakhra555@gmail.com>
2025-10-11 15:12:44 +0530

    ok

--- rpc.cpp
+++ rpc.cpp
@@ -1,3 +1,30 @@
 // rock paper scissor;
-#include<iostream>
-using namespace
+#include <iostream>
+using namespace std;
+int main(){
+    int userchoice , comchoice;
+    
+    // int userchoice;
+    // int comchoice = rand()%3+1;
+    // cout<<rock, paper, scissor ;
+    // cin>> 1.rock  , 2.ppr  , 3.scisoor;
+    // cout<<"enter the choice (1-3)";
+    // cin >> rand()%3+1;
+
+    if(userchoice==comchoice){
+        cout<<"draw match";
+
+        if(userchoice!=comchoice){
+            if(userchoice==1 && comchoice==3);
+            if(userchoice==2 && comchoice==1);
+            if(userchoice==3 && comchoice==2);
+
+            cout<<"user is winner"<< userchoice;
+        }
+        else{
+            cout<<"computer is winner :"<<comchoice<< endl;
+        }
+    }
+    
+    return 0;
+}
